c439a28f284f9563bfd0e714be0a5cfa3730887b
Nino Floris <mail@ninofloris.com>
2023-03-22 18:47:40 +0100

    Add very precisely scoped and combined stats for converters

--- MStatDumper/Program.cs
+++ MStatDumper/Program.cs
@@ -264,7 +264,7 @@ namespace MStatDumper
                     .Take(1000)
                     .ToList();
                 Console.WriteLine("<details>");
-                Console.WriteLine($"<summary>Top 200 Slon Types By Size</summary>");
+                Console.WriteLine("<summary>Top 200 Slon Types By Size</summary>");
                 Console.WriteLine();
                 Console.WriteLine("<br>");
                 Console.WriteLine();
@@ -272,21 +272,246 @@ namespace MStatDumper
                 Console.WriteLine("| --- | --- |");
                 foreach (var m in filteredTypeStats)
                 {
-                    var name = m.Type.Name;
-                    if (m.Type.IsGenericInstance)
-                        name += ((GenericInstanceType)m.Type).GenericArguments.Aggregate(" <",
-                            (s, arg) => s + arg.Name) + ">";
-
-                    name = name.Replace("`", "\\`")
+                    var name = GetConcreteTypeName(m.Type, out _)
+                        .Replace("`", "\\`")
                         .Replace("<", "&#60;")
                         .Replace(">", "&#62;")
                         .Replace("|", "\\|");
+                    ;
                     Console.WriteLine($"| {name} | {m.Size:n0} |");
                 }
                 Console.WriteLine();
                 Console.WriteLine("</details>");
 
+                var filteredConverterTypeStats = GetTypes(types)
+                    .Where(x => x.Type.Namespace.StartsWith("Slon.Pg"))
+                    .ToArray();
+
+                var filteredConverterMethodStats = methodStats
+                    .Where(x => x.Method.DeclaringType.Namespace.StartsWith("Slon.Pg"))
+                    .GroupBy(x => GetCanonTypeName(x.Method.DeclaringType, out _))
+                    .Select(x => KeyValuePair.Create(x.Key, new { Size = x.Sum(x => x.Size + x.GcInfoSize + x.EhInfoSize), DeclaringType = x.First().Method.DeclaringType }))
+                    .ToDictionary(x => x.Key, x => x.Value);
+
+                var intermediateStats =
+                    filteredConverterMethodStats.Join(filteredConverterTypeStats, x => x.Key, stats => GetCanonTypeName(stats.Type, out _), (methodStats, typeStats) =>
+                        {
+                            var type = typeStats.Type;
+                            var canonName = GetCanonTypeName(type, out var hasCanonArg);
+                            var concreteName = GetConcreteTypeName(type, out var concreteIsCanonOrDef);
+
+                            // Whether this type is using shared methods.
+                            var areCanonMethods = !concreteIsCanonOrDef && hasCanonArg;
+                            return new
+                            {
+                                Type = type,
+                                Name = concreteName,
+                                // Whether this type is entirely instantiated over __Canon.
+                                IsCanonTypeInstance = hasCanonArg && concreteIsCanonOrDef,
+                                // Whether this type is actually just a type def.
+                                // we consider these canon for purpose of method size attribution unless both canon and def exists which is handled later on.
+                                IsTypeDef = !hasCanonArg && canonName.Contains("__Canon"),
+                                IsMethodsViaCanon = areCanonMethods,
+                                IsTypeViaCanon = false,
+                                MethodSize = methodStats.Value.Size,
+                                TypeSize = typeStats.Size,
+                            };
+                        })
+                        .UnionBy(filteredConverterTypeStats.Select(x =>
+                        {
+                            var canonName = GetCanonTypeName(x.Type, out var hasCanonArg);
+                            var concreteName = GetConcreteTypeName(x.Type, out var concreteIsCanonOrDef);
+                            return new
+                            {
+                                Type = x.Type,
+                                Name = concreteName,
+                                IsCanonTypeInstance = hasCanonArg && concreteIsCanonOrDef,
+                                IsTypeDef = !hasCanonArg && canonName.Contains("__Canon"),
+                                IsMethodsViaCanon = false,
+                                IsTypeViaCanon = false,
+                                MethodSize = 0,
+                                TypeSize = x.Size,
+                            };
+                        }), x => x.Name)
+                        .ToArray();
+
+                // These are types that have been erased/only have method footprint (boxed_..., static classes, or certain generic instantiations).
+                intermediateStats =
+                    intermediateStats.Concat(
+                        filteredConverterTypeStats.Select(x => x.Type)
+                            .Concat(filteredConverterMethodStats.Select(x => x.Value.DeclaringType))
+                            .Select(x => GetCanonTypeName(x, out _))
+                            .Except(intermediateStats.Select(x => GetCanonTypeName(x.Type, out _)))
+                            .Select(name =>
+                            {
+                                var stats = filteredConverterMethodStats[name];
+                                var canonName = GetCanonTypeName(stats.DeclaringType, out var hasCanonArg);
+                                var genericTypeParams = stats.DeclaringType.IsGenericInstance
+                                    ? ((GenericInstanceType)stats.DeclaringType).GenericArguments.Count
+                                    : stats.DeclaringType.GenericParameters.Count;
+                                var staticCanonName = stats.DeclaringType.Name;
+                                if (genericTypeParams > 0)
+                                    staticCanonName += Enumerable.Range(0, genericTypeParams).Aggregate(" <", (s, _) => s + "__Canon" + ", ")[..^2] + ">";
+                                var concreteName = GetConcreteTypeName(stats.DeclaringType, out var concreteIsCanonOrDef);
+                                var canonType = intermediateStats.FirstOrDefault(x => x.Name == staticCanonName);
+                                return new
+                                {
+                                    Type = stats.DeclaringType,
+                                    Name = concreteName,
+                                    IsCanonTypeInstance = hasCanonArg && concreteIsCanonOrDef,
+                                    IsTypeDef = !hasCanonArg && canonName.Contains("__Canon"),
+                                    IsMethodsViaCanon = false,
+                                    IsTypeViaCanon = canonType is not null,
+                                    MethodSize = stats.Size,
+                                    TypeSize = canonType?.TypeSize ?? 0,
+                                };
+                            })).ToArray();
+
+                var filteredConverterTotalStats =
+                    intermediateStats
+                        .GroupBy(x => GetCanonTypeName(x.Type, out _))
+                        .Select(x =>
+                        {
+                            // There are cases where a canon type only exists as a shared canon, so we add one with AreCanonMethods=false in such a case.
+                            if (x.All(x => x.IsMethodsViaCanon && x.MethodSize > 0))
+                            {
+                                var canonMethod = filteredConverterMethodStats[GetCanonTypeName(x.First().Type, out _)];
+                                var canonMethodName = GetCanonTypeName(canonMethod.DeclaringType, out var hasCanonArg);
+                                var canonConcreteName = GetConcreteTypeName(canonMethod.DeclaringType, out var concreteIsCanonOrDef);
+                                return x.Append(new
+                                {
+                                    Type = canonMethod.DeclaringType,
+                                    Name = canonConcreteName,
+                                    IsCanonTypeInstance = hasCanonArg && concreteIsCanonOrDef,
+                                    IsTypeDef = !hasCanonArg && canonMethodName.Contains("__Canon"),
+                                    IsMethodsViaCanon = false,
+                                    IsTypeViaCanon = false,
+                                    MethodSize = canonMethod.Size,
+                                    TypeSize = 0,
+                                });
+                            }
+
+                            // There are cases where we have both def and canon, only attribute methods to canon in such a case, unless canon has no method size.
+                            if (x.Any(x => x.IsCanonTypeInstance && x.MethodSize > 0) && x.Any(x => x.IsTypeDef && x.MethodSize > 0))
+                            {
+                                return x
+                                    .Select(x => x.IsTypeDef ? x with { MethodSize = 0 } : x);
+                            }
+
+                            return x.AsEnumerable();
+                        }).SelectMany(x => x)
+                        .Select(x =>
+                        {
+                            var typeSize = x.IsTypeViaCanon ? 0 : x.TypeSize;
+                            return new
+                            {
+                                x.Type,
+                                x.Name,
+                                x.IsCanonTypeInstance,
+                                x.IsTypeDef,
+                                x.IsMethodsViaCanon,
+                                x.IsTypeViaCanon,
+                                x.MethodSize,
+                                x.TypeSize,
+                                TotalSize = x.IsMethodsViaCanon ? typeSize : x.MethodSize + typeSize
+                            };
+                        })
+                    .OrderByDescending(x => x.TotalSize)
+                    .ToArray();
+
+                var originalSum = filteredConverterMethodStats.Sum(x => x.Value.Size) + filteredConverterTypeStats.Sum(x => x.Size);
+                var actualSum = filteredConverterTotalStats.Sum(x => x.TotalSize);
+                if (originalSum != actualSum)
+                    throw new InvalidOperationException("Total size of stats is diverging after combining methods and types");
+
+                Console.WriteLine("<details>");
+                Console.WriteLine($"<summary>Converter Namespace Type and Method Size {actualSum:n0}</summary>");
                 Console.WriteLine();
+                Console.WriteLine("<br>");
+                Console.WriteLine();
+                Console.WriteLine("| Name | Type Size | Method Size | Total Size |");
+                Console.WriteLine("| --- | --- | --- | --- |");
+                foreach (var m in filteredConverterTotalStats)
+                {
+                    var name = m.Name.Replace("`", "\\`")
+                        .Replace("<", "&#60;")
+                        .Replace(">", "&#62;")
+                        .Replace("|", "\\|");
+
+                    var msize = m.MethodSize.ToString("n0");
+                    var totalSize = m.TotalSize.ToString("n0");
+                    if (m.IsMethodsViaCanon)
+                    {
+                        msize = $"canon: {m.MethodSize:n0}";
+                    }
+                    else if (m.MethodSize is 0)
+                        msize = "none";
+
+                    var tsize = m.TypeSize.ToString("n0");
+                    if (m.IsTypeViaCanon)
+                        tsize = "canon: " + m.TypeSize;
+                    else if (m.TypeSize is 0)
+                        tsize = "none";
+
+                    Console.WriteLine($"| {name} | {tsize} | {msize} | {totalSize} |");
+                }
+                Console.WriteLine();
+                Console.WriteLine("</details>");
+
+
+                Console.WriteLine();
+            }
+
+            string GetConcreteTypeName(TypeReference type, out bool isCanon)
+            {
+                var name = type.Name;
+                isCanon = false;
+                if (type.IsGenericInstance)
+                {
+                    var canon = true;
+                    name += ((GenericInstanceType)type).GenericArguments.Aggregate(" <",
+                        (s, arg) =>
+                        {
+                            if (canon)
+                                canon = arg.Name == "__Canon";
+                            return s + GetConcreteTypeName(arg, out _) + ", ";
+                        })[..^2] + ">";
+                    isCanon = canon;
+                }
+                else if (type.Name.Contains("`"))
+                    name += " (def)";
+
+                return name;
+            }
+
+            string GetCanonTypeName(TypeReference type, out bool hasCanonArg)
+            {
+                hasCanonArg = false;
+                var name = type.Name;
+                if (type.IsGenericInstance)
+                {
+                    var canon = hasCanonArg;
+                    name += ((GenericInstanceType)type).GenericArguments.Aggregate(" <",
+                        (s, arg) =>
+                        {
+                            var ret = s;
+                            if (arg.IsValueType)
+                                ret += GetCanonTypeName(arg, out var _);
+                            else
+                            {
+                                canon = true;
+                                ret += "__Canon";
+                            }
+
+                            return ret + ", ";
+                        })[..^2] + ">";
+                    hasCanonArg = canon;
+                }
+                else if (type.Name.Contains("`"))
+                    // We gen it but it doesn't exist as an arg.
+                    name += type.GenericParameters.Aggregate(" <", (s, _) => s + "__Canon" + ", ")[..^2] + ">";
+
+                return name;
             }
         }
 
